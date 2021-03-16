1d1e50badeb34f6697660b99daa59103c56b4207
Aulia Rahmi <57357243+arahmi6@users.noreply.github.com>
2021-03-16 09:39:37 +0700

    Menambahkan Cylinder.java dan Rectangle.java
    Mengikuti perintah tugas (init, method double area dan printnya)

---
+++ Painting Shapes/Cylinder.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author AULIA RAHMI
+ */
+public class Cylinder extends Shape {
+    private double radius, height;
+
+    public Cylinder(double r, double h){
+        super("Cylinder");
+        radius = r;
+        height = h;
+    }
+
+    public double area(){
+        return Math.PI*radius*radius*height;
+    }
+
+    public String toString(){
+        return super.toString() + " of radius = " + radius + " and height " + height;
+    } 
+}
---
+++ Painting Shapes/Rectangle.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author AULIA RAHMI
+ */
+public class Rectangle extends Shape {
+    private double length, width;
+
+    public Rectangle(double l, double w){
+        super("Rectangle");
+        length = l;
+        width = w;
+    }
+
+    public double area(){
+        return length*width;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " of length = " + length + " and width = " + width;
+    }  
+}
