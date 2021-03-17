bdf38b3f0aed2973568832fef7496bf395c0aeb3
Aulia Rahmi <57357243+arahmi6@users.noreply.github.com>
2021-03-17 20:49:35 +0700

    Update IntegerList.java
    Final (copy-paste dari tugas sebelumnya)

--- W1/Timing Searching and Sorting Algorithms/IntegerList.java
+++ W1/Timing Searching and Sorting Algorithms/IntegerList.java
@@ -5,7 +5,8 @@
 // sort, and search in a list of integers.  
 //  
 // ****************************************************************  
-import java.util.Scanner;  
+import java.util.Scanner;
+import java.util.*;
 public class IntegerList  {  
     int[] list; //values in the list  
     //------------------------------------------------------------   
@@ -19,17 +20,20 @@ public class IntegerList  {
     // randomize -- fills the array with randomly generated integers   
     // between 1 and 100, inclusive  
     //------------------------------------------------------------   
-    public void randomize()  {  
-        int max = list.length;  
+    public void randomize()  {   
         for (int i=0; i<list.length; i++)  
-        list[i] = (int)(Math.random() * max) + 1;   
-    }  
+            list[i] = (int)(Math.random() * 100) + 1;   
+    }
+    public void print()  {  
+        for (int i=0; i<list.length; i++)  
+            System.out.println(i + ":\t" + list[i]);  
+    }
     //------------------------------------------------------------   
     // fillSorted -- fills the array with sorted values   
     //------------------------------------------------------------   
     public void fillSorted()  {  
         for (int i=0; i<list.length; i++)  
-        list[i] = i + 2;  
+            list[i] = i + 2;  
     }  
     //------------------------------------------------------------   
     // print -- prints array elements with indices, one per line   
@@ -37,7 +41,7 @@ public class IntegerList  {
     public String toString()  {  
         String s = "";  
         for (int i=0; i<list.length; i++)  
-        s += i + ":\t" + list[i] + "\n";  
+            s += i + ":\t" + list[i] + "\n";  
         return s;  
     }  
     //------------------------------------------------------------   
@@ -57,8 +61,49 @@ public class IntegerList  {
     //------------------------------------------------------------   
     public void sortIncreasing()  {  
         for (int i=0; i<list.length-1; i++)  {  
-        int minIndex = minIndex(list, i);  
-        swap(list, i, minIndex);  
-    }  
- }
-
+            int minIndex = minIndex(list, i);  
+            swap(list, i, minIndex);  
+        }
+    }
+    private int minIndex(int[] list, int i) {
+        int minIndex = i;
+        for (int j = i+1; j < list.length; j++){
+            if (list[j] < list[minIndex]){
+                minIndex = j;
+            }
+        }
+        return minIndex;
+    }
+    public void swap(int[] list, int x, int y){
+        int temp = list[x];
+        list[x] = list[y];
+        list[y] = temp;
+    }
+    public int binarySearch(int target){
+        int head = 0;
+        int tail = list.length - 1;
+        while (head <= tail) {
+            int x = head + (tail - head)/2;   
+            if (list[x] < target) {
+                tail = x - 1;
+            } 
+            else if (list[x] > target) {
+                head  = x + 1;
+            } 
+            else {
+                return x;
+            }
+        }
+        return -1;
+    }
+    public void sortDecreasing(){
+        for (int i = 1; i < list.length; i++) {
+            int j = i - 1;
+            while (j >= 0 && list[j] < list[i]) {
+                list[j + 1] = list[j];
+                j = j - 1;
+            }
+            list[j + 1] = list[i];
+        }
+    }
+}
