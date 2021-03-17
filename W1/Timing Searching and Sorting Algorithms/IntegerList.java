7b6c360375f1d03df46c4c8215976fbc48eaf713
Aulia Rahmi <57357243+arahmi6@users.noreply.github.com>
2021-03-17 23:11:58 +0700

    Final
    fix bug

--- W1/Timing Searching and Sorting Algorithms/IntegerList.java
+++ W1/Timing Searching and Sorting Algorithms/IntegerList.java
@@ -104,13 +104,15 @@ public class IntegerList  {
         return -1;
     }
     public void sortDecreasing(){
-        for (int i = 1; i < list.length; i++) {
-            int j = i - 1;
-            while (j >= 0 && list[j] < list[i]) {
-                list[j + 1] = list[j];
-                j = j - 1;
-            }
-            list[j + 1] = list[i];
+        int minIndex;  
+        for (int i=0; i < list.length-1; i++)  {    
+            minIndex = i;  
+            for (int j = i+1; j < list.length; j++)  
+                if (list[j] > list[minIndex])  
+                    minIndex = j;  
+                int temp = list[i]; 
+                list[i] = list[minIndex];  
+                list[minIndex] = temp;  
         }
     }
 }
