a2a267b6163d9d4f05d2b25fa28e089a9f5c46f7
Aulia Rahmi <57357243+arahmi6@users.noreply.github.com>
2021-03-23 14:39:20 +0700

    final
    tambah try catch

--- W2/Placing Exception Handlers/ParseInts.java
+++ W2/Placing Exception Handlers/ParseInts.java
@@ -5,6 +5,7 @@
 //
 // ****************************************************************
 import java.util.Scanner;
+
 public class ParseInts{
     public static void main(String[] args){
         int val, sum=0;
@@ -13,8 +14,12 @@ public class ParseInts{
         System.out.println("Enter a line of text");
         Scanner scanLine = new Scanner(scan.nextLine());
         while (scanLine.hasNext()){
-            val = Integer.parseInt(scanLine.next());
-            sum += val;
+            try{
+                val = Integer.parseInt(scanLine.next());
+                sum += val;                
+            }
+            catch(NumberFormatException e){
+            }
         }
         System.out.println("The sum of the integers on this line is " + sum);
     }
