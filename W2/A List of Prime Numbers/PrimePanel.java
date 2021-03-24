c0c612e25adc7f2955cea9821a1b4acc05dd4f7f
Aulia Rahmi <57357243+arahmi6@users.noreply.github.com>
2021-03-24 11:34:31 +0700

    tambah try catch

--- W2/A List of Prime Numbers/PrimePanel.java
+++ W2/A List of Prime Numbers/PrimePanel.java
@@ -7,6 +7,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+
 public class PrimePanel extends JPanel{
     private JTextField number;
     private JButton computeButton;
@@ -43,34 +44,40 @@ public class PrimePanel extends JPanel{
         // button is clicked.
         // -----------------------------------------------------------
         public void actionPerformed (ActionEvent event){
-            String textNum = number.getText();
-            int num = Integer.parseInt (textNum);
-            String ans = "";
-            int count = 0;
-            if (num < 2)
-                ans = "There no primes less than " + num;
-            else{
-                ans = " " + 2;
-                count++;
-                for (int i = 3; i <= num; i += 2){
-                    boolean foundDivisor = false;
-                    int j = 3;
-                    while (j < i && !foundDivisor){
-                        if (i % j == 0)
-                            foundDivisor = true;
-                        else
-                            j++;
-                    }
-                    // Add i to the list if it is prime
-                    if (j == i){
-                        ans += " " + i;
-                        count++;
-                        if (count % 10 == 0)
-                            ans += "\n";
+            try{
+                String textNum = number.getText();
+                int num = Integer.parseInt (textNum);
+                String ans = "";
+                int count = 0;
+                if (num < 2)
+                    ans = "There no primes less than " + num;
+                else{
+                    ans = " " + 2;
+                    count++;
+                    for (int i = 3; i <= num; i += 2){
+                        boolean foundDivisor = false;
+                        int j = 3;
+                        while (j < i && !foundDivisor){
+                            if (i % j == 0)
+                                foundDivisor = true;
+                            else
+                                j++;
+                        }
+                        // Add i to the list if it is prime
+                        if (j == i){
+                            ans += " " + i;
+                            count++;
+                            if (count % 10 == 0)
+                                ans += "\n";
+                        }
                     }
                 }
+                primeList.setText (ans);  
             }
-            primeList.setText (ans);
+            catch(NumberFormatException n){
+                primeList.setText(n.getMessage()+" : Not Integer!");
+            }
+            
         }
     }
 }
